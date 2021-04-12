45af1acf41f5082165c7c78840a235ee8e64d285
peretarrida13 <pere.tarrida13@gmail.com>
2021-04-12 16:49:05 +0200

    display the graph need to add style

--- covidtracker/src/Components/GraphContainer.js
+++ covidtracker/src/Components/GraphContainer.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useCovidContext } from '../Contexts/contextCovid';
+import LineChart from 'react-linechart';
 
 const flagStyle = {
     height:'15px',
@@ -16,6 +17,8 @@ const inputBox = {
 function GraphContainer() {
     const { fetchCountries, countries} = useCovidContext();
     const [country, setCountry] = useState('Afghanistan');
+    const [countryData, setCountryData] = useState([]);
+    const selected = useRef(null);
 
     async function getUserLocation(position) {
         const url = 'http://ip-api.com/json';
@@ -26,13 +29,45 @@ function GraphContainer() {
         setCountry(data.country);
     }
 
+    async function getCountryData(){
+        const url = 'https://api.covid19api.com/dayone/country/' + country + '/status/confirmed';
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.json();
+        var newData = [];
+        data.map((day, index) => {
+            const newDay =  {
+                x: index,
+                y: day.Cases,
+            }
+            newData.push(newDay);
+        })
+        setCountryData(newData);
+    }
+    
+    const changeCountry = () => {
+        setCountry(selected.current.value);
+    }
+
     useEffect(() => {
         getUserLocation();
     }, []);
 
+    useEffect(() => {
+        getCountryData();
+    }, [country]);
+
+    const config = [
+        {									
+            color: "steelblue", 
+            points: countryData,
+        }
+    ];
+
+
     return(
         <div>
-            <select name='option' style={inputBox}>
+            <select name='option' style={inputBox} ref={selected} onChange={changeCountry}>
                 {countries.map((country) => {
                     return(
                         <option value={country.country} key={country.country}>{country.country}</option>
@@ -40,6 +75,11 @@ function GraphContainer() {
                 })}
             </select>
             <h1>Current Graph: {country}</h1>
+            <LineChart 
+                width={600}
+                height={400}
+                data={config}
+            />
         </div>
     );
 }
